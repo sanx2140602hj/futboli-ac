ad1d5b396c58371fc997f76ab5982b28f2a57a88
sanx2140602hj <92602276+sanx2140602hj@users.noreply.github.com>
2024-01-19 17:14:07 -0600

    avance 19-01-24

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/clubanimalitos'),
+      dir: require('path').join(__dirname, './coverage/Futboli-AC'),
       subdir: '.',
       reporters: [
         { type: 'html' },
