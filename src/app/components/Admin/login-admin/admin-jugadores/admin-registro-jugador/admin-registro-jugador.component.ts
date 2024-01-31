8bb5ab7aa6bfb475fc8f97bae5e7b951b0060278
sanx2140602hj <92602276+sanx2140602hj@users.noreply.github.com>
2024-01-31 00:30:30 -0600

    avance 31-01-24

--- src/app/components/Admin/login-admin/admin-jugadores/admin-registro-jugador/admin-registro-jugador.component.ts
+++ src/app/components/Admin/login-admin/admin-jugadores/admin-registro-jugador/admin-registro-jugador.component.ts
@@ -1,15 +1,107 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-admin-registro-jugador',
   templateUrl: './admin-registro-jugador.component.html',
   styleUrls: ['./admin-registro-jugador.component.css']
 })
 export class AdminRegistroJugadorComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
   }
+  /* Seccion Identificacion */
+  showIdentificacion = true; /*⚠️🦖 este se mantiene en true a diferencia de los demas 
+  componentes porque debe cargar como primera instancia */
+  openIdentificacion() {
+    this.showIdentificacion = true;
+    this.showDatosPersonales = false;
+    this.showContacto = false;
+    this.showDatosFisicos = false;
+    this.showEscolar = false;
+    this.showTutor = false;
+  }
 
+  closeIdentificacion() {
+    this.showIdentificacion = false;
+  }
+
+  /* Seccion Datos personales */
+  showDatosPersonales = false;
+
+  openDatosPersonales() {
+    this.showDatosPersonales = true;
+    this.showIdentificacion = false;
+    this.showContacto = false;
+    this.showDatosFisicos = false;
+    this.showEscolar = false;
+    this.showTutor = false;
+  }
+
+  closeDatosPersonales() {
+    this.showDatosPersonales = false;
+  }
+
+  /* Seccion Contacto */
+  showContacto = false;
+
+  openContacto() {
+    this.showContacto = true;
+    this.showIdentificacion = false;
+    this.showDatosPersonales = false;
+    this.showDatosFisicos = false;
+    this.showEscolar = false;
+    this.showTutor = false;
+  }
+
+  closeContacto() {
+    this.showContacto = false;
+  }
+
+  /* Seccion Datos Fisicos */
+  showDatosFisicos = false;
+
+  openDatosFisicos() {
+    this.showDatosFisicos = true;
+    this.showIdentificacion = false;
+    this.showDatosPersonales = false;
+    this.showContacto = false;
+    this.showEscolar = false;
+    this.showTutor = false;
+  }
+
+  closeDatosFisicos() {
+    this.showDatosFisicos = false;
+  }
+
+  /* Seccion Escolar */
+  showEscolar = false;
+
+  openEscolar() {
+    this.showEscolar = true;
+    this.showIdentificacion = false;
+    this.showDatosPersonales = false;
+    this.showContacto = false;
+    this.showDatosFisicos = false;
+    this.showTutor = false;
+  }
+
+  closeEscolar() {
+    this.showEscolar = false;
+  }
+
+  /* Seccion Tutor */
+  showTutor = false;
+
+  openTutor() {
+    this.showTutor = true;
+    this.showIdentificacion = false;
+    this.showDatosPersonales = false;
+    this.showContacto = false;
+    this.showDatosFisicos = false;
+    this.showEscolar = false;
+  }
+
+  closeTutor() {
+    this.showTutor = false;
+  }
 }
--- src/app/components/Admin/login-admin/admin-jugadores/admin-registro-jugador/identificacion/identificacion.component.ts
+++ src/app/components/Admin/login-admin/admin-jugadores/admin-registro-jugador/identificacion/identificacion.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-identificacion',
   templateUrl: './identificacion.component.html',
   styleUrls: ['./identificacion.component.css']
 })
 export class IdentificacionComponent implements OnInit {
+  @Output() onCloseModal = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  closeModal() {
+    this.onCloseModal.emit();
+  }
+  
 }
