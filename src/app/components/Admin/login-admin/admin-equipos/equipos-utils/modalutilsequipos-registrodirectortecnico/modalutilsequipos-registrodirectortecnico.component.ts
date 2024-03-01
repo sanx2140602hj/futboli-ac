4e93589037d45a7700fba2ff7999537991afc14c
sanx2140602hj <92602276+sanx2140602hj@users.noreply.github.com>
2024-02-29 22:44:03 -0600

    avance 29-02-24V

--- src/app/components/Admin/login-admin/admin-equipos/equipos-utils/modalutilsequipos-registrodirectortecnico/modalutilsequipos-registrodirectortecnico.component.ts
+++ src/app/components/Admin/login-admin/admin-equipos/equipos-utils/modalutilsequipos-registrodirectortecnico/modalutilsequipos-registrodirectortecnico.component.ts
@@ -1,5 +1,5 @@
-// TypeScript (en tu componente)
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-modalutilsequipos-registrodirectortecnico',
@@ -13,6 +13,9 @@ export class ModalutilsequiposRegistrodirectortecnicoComponent implements OnInit
   opciones: string[] = ["Presidente", "Vicepresidente", "Tesorero"]; // Opciones predefinidas
   nuevaOpcion: string = '';
   selectedOption: string = '';
+
+  dirTecControl = new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9 ]*$')]);
+  nuevaOpcionControl = new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9 ]*$')]);
 
   constructor() { }
 
@@ -37,10 +40,29 @@ export class ModalutilsequiposRegistrodirectortecnicoComponent implements OnInit
   }
 
   guardarCambios() {
-    // Lógica para guardar los cambios en el equipo
-    console.log('Nombre del director técnico: ', this.dirTec);
+    // Validar que el primer input tenga al menos un número, letras y espacios
+    const nombreValido = /[a-zA-Z0-9]+/.test(this.dirTec.trim());
+  
+    // Validar que se haya seleccionado un cargo o se haya ingresado uno nuevo
+    const cargoValido = this.selectedOption !== '' || this.nuevaOpcion.trim() !== '';
+  
+    // Verificar que ambos campos sean válidos
+    if (!nombreValido || !cargoValido) {
+      console.error('Error: Todos los campos son requeridos.');
+      return;
+    }
+  
+    // Si los campos son válidos, imprimir en la consola y cerrar el modal
+    console.log('Nombre del director técnico:', this.dirTec);
+    if (this.selectedOption !== '') {
+      console.log('Cargo seleccionado:', this.selectedOption);
+    } else {
+      console.log('Nuevo cargo:', this.nuevaOpcion);
+    }
+  
     this.closeModal(); // Cerrar el modal después de guardar los cambios
   }
+  
 
   opcionSeleccionada() {
     console.log('Nueva opción seleccionada:', this.selectedOption);
