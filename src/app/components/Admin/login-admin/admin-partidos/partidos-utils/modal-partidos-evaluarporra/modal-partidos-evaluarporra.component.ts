55693c8e8b241115621d24f82c60b8099f6abf53
sanx2140602hj <92602276+sanx2140602hj@users.noreply.github.com>
2024-03-07 23:45:41 -0600

    avance 07-03-24v1

--- src/app/components/Admin/login-admin/admin-partidos/partidos-utils/modal-partidos-evaluarporra/modal-partidos-evaluarporra.component.ts
+++ src/app/components/Admin/login-admin/admin-partidos/partidos-utils/modal-partidos-evaluarporra/modal-partidos-evaluarporra.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+
+interface PorraInfo {
+  equipo: string;
+  porra: string;
+}
 
 @Component({
   selector: 'app-modal-partidos-evaluarporra',
   templateUrl: './modal-partidos-evaluarporra.component.html',
   styleUrls: ['./modal-partidos-evaluarporra.component.css']
 })
-export class ModalPartidosEvaluarporraComponent implements OnInit {
+export class ModalPartidosEvaluarporraComponent {
+  @Output() onCloseModal = new EventEmitter<void>();
+  porraInfo: PorraInfo = {
+    equipo: '',
+    porra: ''
+  };
+  porraForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.porraForm = this.fb.group({
+      equipo: ['', Validators.required],
+      porra: ['', Validators.required]
+    });
   }
 
+  guardar() {
+    if (this.porraForm.invalid) {
+      Swal.fire({
+        position: 'top-end',
+        title: 'Operación no realizada',
+        text: 'Por favor, asegúrese de que el formulario esté completo y sin errores',
+        icon: 'error',
+        timer: 2500,
+        showConfirmButton: false
+      });
+    } else {
+      this.porraInfo.equipo = this.porraForm.value.equipo;
+      this.porraInfo.porra = this.porraForm.value.porra;
+
+      console.log('Información de la porra:', this.porraInfo);
+      Swal.fire({
+        position: 'top-end',
+        title: 'Generado con éxito',
+        text: 'Generado con éxito',
+        icon: 'success',
+        timer: 1500,
+        showConfirmButton: false
+      });
+      this.onCloseModal.emit();
+    }
+  }
+
+
+  closeModal() {
+    console.log('Modal cerrado');
+    this.onCloseModal.emit();
+  }
 }
--- src/app/components/Admin/login-admin/admin-partidos/partidos-utils/partidos-utils.component.ts
+++ src/app/components/Admin/login-admin/admin-partidos/partidos-utils/partidos-utils.component.ts
@@ -5,9 +5,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./partidos-utils.component.css']
 })
 export class PartidosUtilsComponent implements OnInit {
+  equipos: string[] = ['equipo 1', 'equipo 2']; // Lista de equipos
+  jugadores: any[] = []; // Lista de jugadores, inicialmente vacía
+  comentario: string = ''; // Variable para almacenar el comentario
   constructor() {}
-  ngOnInit(): void {
+
+  ngOnInit(): void {}
+  selectedEquipo: string = ''; // Variable para almacenar el equipo seleccionado
+
+  onEquipoChange(): void {
+      // Lógica para actualizar la lista de jugadores según el equipo seleccionado
+      if (this.selectedEquipo === '0') {
+          this.jugadores = [
+              { id: '00000', nombre: 'JESUS SANCHEZ HERNANDEZ' },
+              { id: '00001', nombre: 'Otro jugador 1' },
+              // Agrega más jugadores según sea necesario
+          ];
+      } else if (this.selectedEquipo === '1') {
+          this.jugadores = [
+              { id: '00003', nombre: 'Juan Carlos' },
+              { id: '00004', nombre: 'Otro jugador 2' },
+              // Agrega más jugadores según sea necesario
+          ];
+      } else {
+          this.jugadores = []; // Limpiar la lista si no se selecciona ningún equipo
+      }
   }
+ /* -------------------------- */
+ guardarAsistencia(jugador: any): void {
+  console.log('Guardando asistencia para el jugador:', jugador);
+  // Aquí puedes agregar la lógica para guardar la asistencia del jugador en tu backend o donde sea necesario
+}
+
+ /* -------------------------- */
+//comentario
+guardarComentario(): void {
+  console.log('Comentario guardado:', this.comentario);
+  // Aquí puedes agregar la lógica para guardar el comentario en tu backend o donde sea necesario
+}
  /* -------------------------- */
   //DATOS
   openModalAgregarDATOS = false;
