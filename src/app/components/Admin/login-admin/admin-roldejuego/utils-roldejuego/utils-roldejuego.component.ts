8bbf60f749eb3e2aca4afc7d747483ee1a8c17fb
sanx2140602hj <92602276+sanx2140602hj@users.noreply.github.com>
2024-03-08 20:41:05 -0600

    avance 08-03-24c1

--- src/app/components/Admin/login-admin/admin-roldejuego/utils-roldejuego/utils-roldejuego.component.ts
+++ src/app/components/Admin/login-admin/admin-roldejuego/utils-roldejuego/utils-roldejuego.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UtilsRoldejuegoComponent implements OnInit {
   equipos: string[] = ['Equipo 1', 'Equipo 2']; // Variable para almacenar los nombres de los equipos
-  cantidadSelects: number = 5; // Variable para definir la cantidad de selects a generar
+  cantidadSelects: number = 7; // Variable para definir la cantidad de selects a generar
   temporadas: string[] = ['Temporada 1', 'Temporada 2', 'Temporada 3', 'Temporada 4']; // Variable para almacenar las temporadas
   equipoTables: string[] = ['Mexico', 'EEUU', 'España', 'Italia']; // Variable para almacenar los nombres de los equipos en la tabla
 
