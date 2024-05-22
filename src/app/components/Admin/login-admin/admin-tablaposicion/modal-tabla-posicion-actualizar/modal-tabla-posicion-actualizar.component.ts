02c6b362bdfac101d82bed364ab0606019e4d2d5
sanx2140602hj <92602276+sanx2140602hj@users.noreply.github.com>
2024-05-21 22:03:40 -0600

    codigo extra en api-4.1

--- src/app/components/Admin/login-admin/admin-tablaposicion/modal-tabla-posicion-actualizar/modal-tabla-posicion-actualizar.component.ts
+++ src/app/components/Admin/login-admin/admin-tablaposicion/modal-tabla-posicion-actualizar/modal-tabla-posicion-actualizar.component.ts
@@ -1,34 +1,36 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import Swal from 'sweetalert2';
 import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-modal-tabla-posicion-actualizar',
   templateUrl: './modal-tabla-posicion-actualizar.component.html',
   styleUrls: ['./modal-tabla-posicion-actualizar.component.css']
 })
 export class ModalTablaPosicionActualizarComponent implements OnInit {
-  @Input() categoryId: number | null = null; // Recibir el ID de la categoría como entrada
-  comentario: string = '';
-  data: any = {};
-  editarDatos: any = {};
-  IDprueba: any = {};
   miFormulario: FormGroup;
-  dateDescription: any = {};
-  error: boolean = false; // Variable para controlar si hay error
-  palabraClave: string = '';
+
 
   constructor(private fb: FormBuilder, private http: HttpClient) {
     this.miFormulario = this.fb.group({
-      goleo: [0, [Validators.required, Validators.min(0)]],
-      amonestacion: [0, [Validators.required, Validators.min(0)]],
-      tarjetaRoja: [0, [Validators.required, Validators.min(0)]]
+      goleo: [0, [Validators.required, this.validateNumber]],
+      amonestacion: [0, [Validators.required, this.validateNumber]],
+      tarjetaRoja: [0, [Validators.required, this.validateNumber]]
     });
   }
 
   ngOnInit(): void {
   }
+
+  validateNumber(control: AbstractControl) {
+    const value = control.value;
+    if (isNaN(value) || !Number.isInteger(value) || value < 1 || value > 99) {
+      return { invalidNumber: true };
+    }
+    return null;
+  }
+
   get goleo() {
     return this.miFormulario.get('goleo');
   }
@@ -42,10 +44,21 @@ export class ModalTablaPosicionActualizarComponent implements OnInit {
   }
 
   @Output() onCloseModal = new EventEmitter<void>();
-  guardarModal() {
-    console.log("Hola");
-  }
 
+  guardarModal() {
+    if (this.miFormulario.valid) {
+      const goleo = this.miFormulario.value.goleo;
+      const amonestacion = this.miFormulario.value.amonestacion;
+      const tarjetaRoja = this.miFormulario.value.tarjetaRoja;
+
+      console.log("Valores ingresados:", "Goleo: ",goleo, "Amonestacion: ",amonestacion, "tarjetaRoja: ",tarjetaRoja);
+
+
+    } else {
+      // Mostrar mensaje de error si el formulario no es válido
+      Swal.fire('Error', 'Por favor, completa el formulario correctamente', 'error');
+    }
+  }
 
   closeModal() {
     console.log('Modal cerrado');
